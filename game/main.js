205328e48f380fa008eeede2069142bd5b3d6f10
Phoenix <48478854+ItsTheChickenMan@users.noreply.github.com>
2019-10-31 09:39:13 -0400

    Update main.js

--- game/main.js
+++ game/main.js
@@ -1,10 +1,12 @@
-var clickListener = document.getElementById("module");
 lockMouse = function(){
   var img = document.createElement("image");
+  document.body.appendChild(img);
   img.src = "cursor.png";
   img.x = event.clientX;
   img.y = event.clientY;
   var sty = document.getElementByTagName("style");
   sty.innerHTML = "html { cursor: none; }";
 }
-clickListener.onclick = lockMouse();
+document.addEventListener("click", function(){
+  lockMouse();
+});
