1e442c582967ce8d915683cc75a5ebd61295bf01
Phoenix <48478854+ItsTheChickenMan@users.noreply.github.com>
2019-11-01 09:25:07 -0400

    Update main.js

--- game/main.js
+++ game/main.js
@@ -1,7 +1,7 @@
 function lockMouse(e) {
   var img = document.createElement("image");
   img.src = "cursor.png";
-  img.style = "position: absolute; left: " + e.x + "; up: " + e.y + ";";
+  img.style = "position: absolute; left: " + e.client.x + "; up: " + e.client.y + ";";
   document.getElementById("body").appendChild(img);
   var sty = document.getElementById("sty");
   sty.innerHTML = `html {
